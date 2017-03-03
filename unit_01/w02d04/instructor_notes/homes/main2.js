d146e4aee345050d5998f60bf60b0e37de087dfb
deviea8 <deviea8@gmail.com>
2017-03-02 20:48:37 -0500

    updated main2.js file

--- unit_01/w02d04/instructor_notes/homes/main2.js
+++ unit_01/w02d04/instructor_notes/homes/main2.js
@@ -13,22 +13,28 @@ var removeRow = function() {
     })
   }
 
-  var newHomes = [
-      {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
-      {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
-      {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
-      {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
-  ];
+// Object of other addresses.
+
+var newHomes = [
+  {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
+  {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
+  {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
+  {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
+];
+
 
 var tableTbody = $('table tbody')
-
 var counter = 0;
+
+// To populate new rows in the table, I created 4 variables to grab key-values in the newHomes object.
 
 var newRow1 = $('<tr><td>' + newHomes[0].address + '</td><td>' + newHomes[0].sf + '</td><td>' + newHomes[0].bedrooms + '</td><td>' + newHomes[0].baths + '</td><td>' + newHomes[0].price + '</td><td><button class="btn btn-xs btn-danger">Remove</button></td></tr>');
 var newRow2 = $('<tr><td>' + newHomes[1].address + '</td><td>' + newHomes[1].sf + '</td><td>' + newHomes[1].bedrooms + '</td><td>' + newHomes[1].baths + '</td><td>' + newHomes[1].price + '</td><td><button class="btn btn-xs btn-danger">Remove</button></td></tr>');
 var newRow3 = $('<tr><td>' + newHomes[2].address + '</td><td>' + newHomes[2].sf + '</td><td>' + newHomes[2].bedrooms + '</td><td>' + newHomes[2].baths + '</td><td>' + newHomes[2].price + '</td><td><button class="btn btn-xs btn-danger">Remove</button></td></tr>');
 var newRow4 = $('<tr><td>' + newHomes[3].address + '</td><td>' + newHomes[3].sf + '</td><td>' + newHomes[3].bedrooms + '</td><td>' + newHomes[3].baths + '</td><td>' + newHomes[3].price + '</td><td><button class="btn btn-xs btn-danger">Remove</button></td></tr>');
 
+
+// Created functions to add new rows to table based on counter (number of clicks) value.
 
 var addNewHome = function() {
   if (counter === 0) {
@@ -61,7 +67,8 @@ var addNewHome = function() {
   }
 }
 
-$('#addHome').on('click', addNewHome)
+// Click listener on button.
 
+$('#addHome').on('click', addNewHome)
 
 })
