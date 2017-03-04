b109a04aab35fff836ec700039218b1a1575e1e6
deviea8 <deviea8@gmail.com>
2017-03-04 17:21:36 -0500

    completed stopwatch homework

--- unit_01/w02d03/homework/hw.js
+++ unit_01/w02d03/homework/hw.js
@@ -3,6 +3,7 @@
 // See LICENSE for details.
 
 /// Data & Core Business Logic ///
+
 const Stopwatch = {
   tickClock: function(){
     if (Stopwatch.isRunning) {
@@ -18,29 +19,47 @@ const Stopwatch = {
   laps: [],
   // DO NOT EDIT ABOVE THIS LINE
   advanceTenMillisecs: function(){
-    // Your Code Here
+    Stopwatch.millisecs += 10;
+    ViewEngine.updateTimeDisplay();
+    if (Stopwatch.millisecs === 1000) {
+      Stopwatch.millisecs = 0;
+      Stopwatch.secs += 1;
+    }
+    if (Stopwatch.secs === 60) {
+      Stopwatch.secs = 0;
+      Stopwatch.mins += 1;
+    }
   },
   reset: function(){
-    // Your Code Here
+    Stopwatch.mins = 0;
+    Stopwatch.secs = 0;
+    Stopwatch.millisecs = 0;
+    ViewEngine.updateTimeDisplay();
   },
   start: function(){
-    // Your Code Here
+    Stopwatch.isRunning = true;
+    Stopwatch.tickClock();
   },
   stop: function(){
-    // Your Code Here
+    Stopwatch.isRunning = false;
   },
   lap: function(){
-    // Your Code Here
+    Stopwatch.laps.push(document.getElementById('time-display').innerHTML);
+    ViewEngine.updateLapListDisplay();
   }
 };
+
 
 /// User Interface ///
 const ViewEngine = {
   updateTimeDisplay: function(mins, secs, millisecs){
-    // Your Code Here
+    $('#millisecs').text(Stopwatch.millisecs)
+    $('#secs').text(Stopwatch.secs)
+    $('#mins').text(Stopwatch.mins)
   },
   updateLapListDisplay: function(laps){
-    // Your Code Here
+    console.log('update lap list triggered')
+    $('#lap-list').append("<li>" + Stopwatch.laps[Stopwatch.laps.length - 1] + "</li>")
   },
 };
 const ViewHelpers = {
@@ -52,19 +71,42 @@ const ViewHelpers = {
 /// Top-Level Application Code ///
 const AppController = {
   handleClockTick: function(){
-    // Your Code Here
   },
   handleClickStart: function() {
-    // Your Code Here
+    console.log('handleClickStart fired')
+    if (Stopwatch.isRunning === true) {
+      console.log('Stopwatch is currently running')
+    }
+    else {
+      Stopwatch.start();
+    }
   },
   handleClickStopReset: function(){
-    // Your Code Here
+    console.log('handleClickStopReset fired')
+    if (Stopwatch.isRunning === true) {
+      Stopwatch.stop();
+    }
+    else {
+      Stopwatch.reset();
+    }
   },
   handleClickLap: function(){
-    // Your Code Here
+    if (Stopwatch.isRunning === true) {
+      Stopwatch.lap();
+    }
+    else {
+      console.log('Stopwatch not running - no lap to display');
+    }
   }
 };
 
+
+
 window.onload = function(){
-  // Attach AppController methods to the DOM as event handlers here.
+  console.log('window loaded')
+
+$('#start').on('click', AppController.handleClickStart);
+$('#lap').on('click', AppController.handleClickLap);
+$('#stop').on('click', AppController.handleClickStopReset);
+
 };
