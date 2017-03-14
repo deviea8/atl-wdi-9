68aa379f7c1e05d431d9bfcf6ba24ed950ee77fd
deviea8 <deviea8@gmail.com>
2017-03-14 11:25:00 -0400

    Successful pirate joke

--- labs/node-lab/hello-express/server.js
+++ labs/node-lab/hello-express/server.js
@@ -1,13 +1,30 @@
 var express = require('express'); // Loading the express module on our server
 var app = express(); // Create a new instance of express on our server
+var hbs = require('hbs'); // loads handlebars
 
+
+app.use(express.static(__dirname + '/public')); // VERY IMPORTANT!! Make sure to add a '/'
+
+app.set("view engine", "hbs"); //tells Express what to use for rendering templates
 
 app.get('/', function(req, res) { // when a request comes in at localhost:3000/ it will respond
   res.send('Hello friend!');
 });
 
 app.get('/greeting', function(req, res) { // when a request comes in at localhost:3000/ it will respond
-  res.send('HEY, WDI 9!');
+  // res.send('HEY, WDI 9!');
+  console.log(req.query);
+  res.render('greeting', {
+    data: req.query.saying
+  });
+});
+
+app.get('/favorite-foods', function(req, res) {
+    var favoriteFoods = ["Jeni's almond butter brittle ice cream", 'Tacos from Superica', 'a breakfast sandwich from Gjelina to go in Venice', 'Croissants from Bottega Louie in downtown Los Angeles', 'Pizza from Little Star in San Francisco'];
+
+    res.render('favorite-foods', {
+        data: favoriteFoods
+    });
 });
 
 app.get('/sightings', function(req, res) { // when a request comes in at localhost:3000/ it will respond
@@ -16,13 +33,13 @@ app.get('/sightings', function(req, res) { // when a request comes in at localho
   res.send(`How many ufo sightings you think there are in ${req.query.state}? ${req.query.sights}.`);
 });
 
-app.get('/favorite/:noun', function(req, res) { // when a request comes in at localhost:3000/ it will respond
-  console.log(req.route);
-  console.log(req.params);
-  console.log(req.query);
-  res.send(`I have a favorite ${req.params.noun}. It is ${req.query.color}.`);
-  console.log({params: req.params, queries: req.query});
-});
+// app.get('/favorite/:noun', function(req, res) { // when a request comes in at localhost:3000/ it will respond
+//   console.log(req.route);
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.send(`I have a favorite ${req.params.noun}. It is ${req.query.color}.`);
+//   console.log({params: req.params, queries: req.query});
+// });
 
 app.get('/bigfoot', function(req, res) { // when a request comes in at localhost:3000/ it will respond
   console.log(req.route);
