6ea8eb4c572fac82ffbe71808c103360418f3636
deviea8 <deviea8@gmail.com>
2017-03-15 17:28:43 -0400

    Completed GOOD OLD FORM

--- unit_02/w06d03/homework/program.js
+++ unit_02/w06d03/homework/program.js
@@ -20,14 +20,39 @@
 
 // start level: STATIC
 
+// // packages
+// var express = require('express');
+
+// // app settings
+// var app = express();
+// var port = process.env.PORT || process.argv[2];
+
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+
+// // start server
+// app.listen(port, function() {
+//   console.log('ready');
+// })
+
+// start level: GOOD OLD FORM
+
 // packages
 var express = require('express');
+var bodyparser = require('body-parser')
 
 // app settings
 var app = express();
 var port = process.env.PORT || process.argv[2];
 
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+app.use(bodyparser.urlencoded({extended: true}))
+
+// form
+app.post('/form', function(req, res) {
+  var reverseInput = function() {
+    return req.body.str.split('').reverse().join('');
+  }
+  res.send(reverseInput());
+})
 
 // start server
 app.listen(port, function() {
