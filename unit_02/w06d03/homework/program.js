be1a6b18c64a3f651c1e9956d03e5bce69ec91e4
deviea8 <deviea8@gmail.com>
2017-03-15 17:48:30 -0400

    Completed WHATS IN QUERY lesson

--- unit_02/w06d03/homework/program.js
+++ unit_02/w06d03/homework/program.js
@@ -60,28 +60,43 @@
 // })
 
 
-// start level: PARAM PAM PAM
+// // start level: PARAM PAM PAM
+// // packages
+// var express = require('express');
+// var bodyparser = require('body-parser')
+// var crypto = require('crypto')
+
+// // app settings
+// var app = express();
+// var port = process.env.PORT || process.argv[2];
+
+// app.use(bodyparser.urlencoded({extended: true}))
+
+// app.put ('/message/:id', function (req, res) {
+//   var userInputId = req.params.id;
+//   res.send(crypto.createHash('sha1').update(new Date().toDateString() + userInputId).digest('hex'));
+// });
+
+// // start server
+// app.listen(port, function() {
+//   console.log('ready');
+// })
+
+
+// start level: WHATS IN QUERY
 // packages
 var express = require('express');
-var bodyparser = require('body-parser')
-var crypto = require('crypto')
 
 // app settings
 var app = express();
 var port = process.env.PORT || process.argv[2];
 
-app.use(bodyparser.urlencoded({extended: true}))
-
-app.put ('/message/:id', function (req, res) {
-  var userInputId = req.params.id;
-  res.send(crypto.createHash('sha1').update(new Date().toDateString() + userInputId).digest('hex'));
-});
+app.get('/search', function(req, res) {
+  res.send(req.query)
+})
 
 // start server
 app.listen(port, function() {
   console.log('ready');
 })
-
-
-
 
