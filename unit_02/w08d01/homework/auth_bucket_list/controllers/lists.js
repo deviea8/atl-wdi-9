2c4a65ab97f1a491e0a1901df2cd371a39f61bb0
deviea8 <deviea8@gmail.com>
2017-03-23 23:37:09 -0400

    added buttons to show page but they dont work

--- unit_02/w08d01/homework/auth_bucket_list/controllers/lists.js
+++ unit_02/w08d01/homework/auth_bucket_list/controllers/lists.js
@@ -37,8 +37,8 @@ router.post('/', function createNewBucketListItem(req, res){
 // EDIT
 //======================
 //create a GET "/:id/edit" route that renders the list's edit page
-router.get('/:id/edit', function(req, res){
-  User.findById(req.params.userId)
+router.get('/:itemId/edit', function(req, res){
+  User.findById(req.params.id)
   .exec(function(err,user){
     if(err) {console.log(err)}
     res.send('hello')
@@ -57,7 +57,21 @@ router.get('/:id/edit', function(req, res){
 // DELETE
 //======================
 //create a DELETE "/:id" route that deletes the list item
+router.delete('/:itemId', function deleteBucketListItem(req, res) {
+  User.findById(req.params.id)
+    .exec(function (err, user){
+      if (err) { console.log(err); }
 
+      user.list.itemId(req.params.itemId).remove();
+
+      user.save(function (err) {
+        if (err) console.log(err);
+        console.log('Bucket list item was removed')
+      });
+
+      res.redirect('users/' + id)
+      });
+    });
 
 //======================
 // EXPORTS
--- unit_02/w08d01/homework/auth_bucket_list/controllers/users.js
+++ unit_02/w08d01/homework/auth_bucket_list/controllers/users.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res) {
   .exec(function(err, users){
     if (err) { console.log(err); }
     res.render('users/index', {
-      users: users
+      users: users,
     });
   });
 });
